7752e98066d76afd0924a031750abdf9abe9fe1c
ihsanuddienullah <ihsanuddienullah@gmail.com>
2022-04-23 18:55:13 +0700

    add packages needs

---
+++ src/pages/detail-seller/detail-seller.container.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DetailSeller() {
+  return (
+    <div>DetailSeller</div>
+  );
+}
+
+export default DetailSeller;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from './home/home.container';
+import DetailSeller from './detail-seller/detail-seller.container';
+
+export { Home, DetailSeller };
