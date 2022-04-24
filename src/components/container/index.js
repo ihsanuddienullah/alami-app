f50d8519231d0104016fefdd9cded0c0d61fe7e7
ihsanuddienullah <ihsanuddienullah@gmail.com>
2022-04-24 17:20:18 +0700

    rename folder

---
+++ src/components/container/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ContainerPage } from './container.style';
+
+const Container = ({ children }) => (
+  <ContainerPage>{children}</ContainerPage>
+);
+
+export default Container;
