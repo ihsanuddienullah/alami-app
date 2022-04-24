bca5e7ae9f1b3c0bc25f6bd26d498d30ca0034fa
ihsanuddienullah <ihsanuddienullah@gmail.com>
2022-04-24 17:19:46 +0700

    rename folder

--- src/components/Container/container.style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const ContainerPage = styled.div`
-    width: 70%;
-    margin: 0 auto;
-`;
--- src/components/Container/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ContainerPage } from './container.style';
-
-const Container = ({ children }) => (
-  <ContainerPage>{children}</ContainerPage>
-);
-
-export default Container;
